2f4047415cd33afc5b3ff08b8097c3c1bb79f735
Dharmesh Bhuva <bhuva.d@wehi.edu.au>
2021-01-21 00:38:19 +1100

    Added data for latest version of MSigDB (i.e. v7.2).

--- inst/scripts/make-data.R
+++ inst/scripts/make-data.R
@@ -1,29 +1,63 @@
 library(GSEABase)
 library(BiocFileCache)
 
-msigdb_url = 'https://data.broadinstitute.org/gsea-msigdb/msigdb/release/7.1/msigdb_v7.1.xml'
-
-#download file
 fpath = tempfile()
 bfc = BiocFileCache(fpath, ask = FALSE)
-msigpath = bfcrpath(bfc, msigdb_url)
 
-#----MSigDB v7.1 Symbols----
-#read genesets into a geneset collection
-msigdb = getBroadSets(msigpath, membersId = 'MEMBERS_SYMBOLIZED')
+getMsigdbData <- function(version) {
+  msigdb_url = 'https://data.broadinstitute.org/gsea-msigdb/msigdb/release/___/msigdb_v___.xml'
+  msigdb_url = gsub('___', version, msigdb_url)
+  
+  #download file
+  msigpath = bfcrpath(bfc, msigdb_url)
+  
+  #----Symbols----
+  #read genesets into a geneset collection
+  msigdb = getBroadSets(msigpath, membersId = 'MEMBERS_SYMBOLIZED')
+  msigdb = GeneSetCollection(msigdb[!sapply(lapply(msigdb, collectionType), bcSubCategory) %in% 'CP:KEGG'])
+  
+  #gene set modifications
+  msigdb = GeneSetCollection(lapply(msigdb, function(gs) {
+    #add URLs
+    urls(gs) = msigdb_url
+    return(gs)
+  }))
+  
+  #remove empty gene sets
+  msigdb_lengths = sapply(lapply(msigdb, geneIds), length)
+  msigdb = GeneSetCollection(msigdb[msigdb_lengths > 0])
+  msigdb.sym = msigdb
+  
+  #----Entrez IDs----
+  #read genesets into a geneset collection
+  msigdb = getBroadSets(msigpath, membersId = 'MEMBERS_EZID')
+  msigdb = GeneSetCollection(msigdb[!sapply(lapply(msigdb, collectionType), bcSubCategory) %in% 'CP:KEGG'])
+  
+  #gene set modifications
+  msigdb = GeneSetCollection(lapply(msigdb, function(gs) {
+    #convert idType
+    gs@geneIdType = EntrezIdentifier()
+    #add URLs
+    urls(gs) = msigdb_url
+    return(gs)
+  }))
+  
+  #remove empty gene sets
+  msigdb_lengths = sapply(lapply(msigdb, geneIds), length)
+  msigdb = GeneSetCollection(msigdb[msigdb_lengths > 0])
+  msigdb.ezid = msigdb
+  
+  return(list(msigdb.sym, msigdb.ezid))
+}
 
-#remove empty gene sets
-msigdb_lengths = sapply(lapply(msigdb, geneIds), length)
-msigdb = GeneSetCollection(msigdb[msigdb_lengths > 0])
-msigdb.v7.1.SYM = msigdb
-save(msigdb.v7.1.SYM, file = 'msigdb.v7.1.SYM.rda')
+v7.1 = getMsigdbData('7.1')
+msigdb.hs.v7.1.SYM = v7.1[[1]]
+msigdb.hs.v7.1.EZID = v7.1[[2]]
+save(msigdb.hs.v7.1.SYM, file = 'msigdb.hs.v7.1.SYM.rda')
+save(msigdb.hs.v7.1.EZID, file = 'msigdb.hs.v7.1.EZID.rda')
 
-#----MSigDB v7.1 Entrez IDs----
-#read genesets into a geneset collection
-msigdb = getBroadSets(msigpath, membersId = 'MEMBERS_EZID')
-
-#remove empty gene sets
-msigdb_lengths = sapply(lapply(msigdb, geneIds), length)
-msigdb = GeneSetCollection(msigdb[msigdb_lengths > 0])
-msigdb.v7.1.EZID = msigdb
-save(msigdb.v7.1.EZID, file = 'msigdb.v7.1.EZID.rda')
+v7.2 = getMsigdbData('7.2')
+msigdb.hs.v7.2.SYM = v7.2[[1]]
+msigdb.hs.v7.2.EZID = v7.2[[2]]
+save(msigdb.hs.v7.2.SYM, file = 'msigdb.hs.v7.2.SYM.rda')
+save(msigdb.hs.v7.2.EZID, file = 'msigdb.hs.v7.2.EZID.rda')
--- inst/scripts/make-metadata.R
+++ inst/scripts/make-metadata.R
@@ -1,25 +1,34 @@
 meta = data.frame(
-  Title = c('msigdb.hs.v7.1.SYM', 'msigdb.hs.v7.1.EZID'),
+  Title = c('msigdb.hs.v7.1.SYM', 'msigdb.hs.v7.1.EZID', 'msigdb.hs.v7.2.SYM', 'msigdb.hs.v7.2.EZID'),
   Description = c(
-    'Gene expression signatures from the Molecular Signatures Database (v7.1). Signatures are represented using gene symbols.',
-    'Gene expression signatures from the Molecular Signatures Database (v7.1). Signatures are represented using Entrez IDs.'
+    'Gene expression signatures from the Molecular Signatures Database (v7.1) excluding KEGG gene sets. Signatures are represented using gene symbols.',
+    'Gene expression signatures from the Molecular Signatures Database (v7.1) excluding KEGG gene sets. Signatures are represented using Entrez IDs.',
+    'Gene expression signatures from the Molecular Signatures Database (v7.2) excluding KEGG gene sets. Signatures are represented using gene symbols.',
+    'Gene expression signatures from the Molecular Signatures Database (v7.2) excluding KEGG gene sets. Signatures are represented using Entrez IDs.'
   ),
-  BiocVersion = c(3.13, 3.13),
+  BiocVersion = c(3.13, 3.13, 3.13, 3.13),
   Genome = NA,
   SourceType = c('XML'),
   SourceUrl = c(
     'https://data.broadinstitute.org/gsea-msigdb/msigdb/release/7.1/msigdb_v7.1.xml',
-    'https://data.broadinstitute.org/gsea-msigdb/msigdb/release/7.1/msigdb_v7.1.xml'
+    'https://data.broadinstitute.org/gsea-msigdb/msigdb/release/7.1/msigdb_v7.1.xml',
+    'https://data.broadinstitute.org/gsea-msigdb/msigdb/release/7.2/msigdb_v7.2.xml',
+    'https://data.broadinstitute.org/gsea-msigdb/msigdb/release/7.2/msigdb_v7.2.xml'
   ),
-  SourceVersion = c('7.1', '7.1'),
-  Species = c('Homo sapiens', 'Homo sapiens'),
-  TaxonomyId = c(9606, 9606),
+  SourceVersion = c('7.1', '7.1', '7.2', '7.2'),
+  Species = c('Homo sapiens', 'Homo sapiens', 'Homo sapiens', 'Homo sapiens'),
+  TaxonomyId = c(9606, 9606, 9606, 9606),
   Coordinate_1_based = TRUE,
   DataProvider = c('Broad Institute'),
   Maintainer = 'Dharmesh D. Bhuva <bhuva.d@wehi.edu.au>',
   RDataClass = 'GSEABase::GeneSetCollection',
   DispatchClass = 'Rda',
-  RDataPath = c('msigdb/msigdb.hs.v7.1.SYM.rda', 'msigdb/msigdb.hs.v7.1.EZID.rda')
+  RDataPath = c(
+    'msigdb/msigdb.hs.v7.1.SYM.rda',
+    'msigdb/msigdb.hs.v7.1.EZID.rda',
+    'msigdb/msigdb.hs.v7.2.SYM.rda',
+    'msigdb/msigdb.hs.v7.2.EZID.rda'
+  )
 )
 
 write.csv(meta, file = 'inst/extdata/metadata.csv', row.names = FALSE)
