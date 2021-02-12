e6e65e7f4f18a6933ff11d37895e7945aef74e85
bhuvad <bhuva.d@wehi.edu.au>
2021-02-12 22:20:07 +1100

    Added metadata record for the new mouse MSigDB.

--- inst/scripts/make-metadata.R
+++ inst/scripts/make-metadata.R
@@ -1,19 +1,23 @@
 meta = data.frame(
   Title = c('msigdb.hs.SYM', 'msigdb.hs.EZID'),
   Description = c(
-    'Gene expression signatures from the Molecular Signatures Database (v7.2) excluding KEGG gene sets. Signatures are represented using gene symbols.',
-    'Gene expression signatures from the Molecular Signatures Database (v7.2) excluding KEGG gene sets. Signatures are represented using Entrez IDs.'
+    'Gene expression signatures (human) from the Molecular Signatures Database (v7.2) excluding KEGG gene sets. Signatures are represented using gene symbols.',
+    'Gene expression signatures (human) from the Molecular Signatures Database (v7.2) excluding KEGG gene sets. Signatures are represented using Entrez IDs.',
+    'Gene expression signatures (mouse) from the Molecular Signatures Database (v7.2) excluding KEGG gene sets. Signatures are represented using gene symbols.',
+    'Gene expression signatures (mouse) from the Molecular Signatures Database (v7.2) excluding KEGG gene sets. Signatures are represented using Entrez IDs.'
   ),
-  BiocVersion = c(3.13, 3.13),
+  BiocVersion = c(3.13, 3.13, 3.13, 3.13),
   Genome = NA,
   SourceType = c('XML'),
   SourceUrl = c(
     'https://data.broadinstitute.org/gsea-msigdb/msigdb/release/7.2/msigdb_v7.2.xml',
+    'https://data.broadinstitute.org/gsea-msigdb/msigdb/release/7.2/msigdb_v7.2.xml',
+    'https://data.broadinstitute.org/gsea-msigdb/msigdb/release/7.2/msigdb_v7.2.xml',
     'https://data.broadinstitute.org/gsea-msigdb/msigdb/release/7.2/msigdb_v7.2.xml'
   ),
-  SourceVersion = c('7.2', '7.2'),
-  Species = c('Homo sapiens', 'Homo sapiens'),
-  TaxonomyId = c(9606, 9606),
+  SourceVersion = c('7.2', '7.2', '7.2', '7.2'),
+  Species = c('Homo sapiens', 'Homo sapiens', 'Mus musculus', 'Mus musculus'),
+  TaxonomyId = c(9606, 9606, 10090, 10090),
   Coordinate_1_based = TRUE,
   DataProvider = c('Broad Institute'),
   Maintainer = 'Dharmesh D. Bhuva <bhuva.d@wehi.edu.au>',
@@ -21,7 +25,9 @@ meta = data.frame(
   DispatchClass = 'Rda',
   RDataPath = c(
     'msigdb/msigdb.hs.SYM.rda',
-    'msigdb/msigdb.hs.EZID.rda'
+    'msigdb/msigdb.hs.EZID.rda',
+    'msigdb/msigdb.mm.SYM.rda',
+    'msigdb/msigdb.mm.EZID.rda'
   )
 )
 
