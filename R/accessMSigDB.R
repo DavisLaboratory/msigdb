e370c35169ef94a1d8d0def138f75b0113b510df
Dharmesh Bhuva <bhuva.d@wehi.edu.au>
2021-03-19 21:58:38 +1100

    Added function to retrieve the correct object from the Hub.

--- R/accessMSigDB.R
+++ R/accessMSigDB.R
@@ -1,3 +1,34 @@
+#' Retrieve MSigDB data hosted on the hub
+#'
+#' Download molecular signatures database (MSigDB) hosted on the ExperimentHub
+#' or retrieve pre-downloaded version from cache. This package currently hosts
+#' versions greater than 7.2 for human and mouse with both symbol and Entrez
+#' identifiers.
+#'
+#' @param org a character, representing the organism whose signature database
+#'   needs to be retrieved ("hs" for human and "mm" for mouse).
+#' @param id a character, representing the ID type to use ("SYM" for gene
+#'   symbols and "EZID" for Entrez IDs).
+#' @param version a numeric, stating the version of MSigDB to be retrieved
+#'   (should be >= 7.2).
+#'
+#' @return a GeneSetCollection, containing GeneSet objects from the specified
+#'   version of the molecular signatures database (MSigDB).
+#' @export
+#'
+#' @examples
+#' gsc = getMsigdb('hs', 'SYM')
+#' 
+getMsigdb <- function(org = c('hs', 'mm'), id = c('SYM', 'EZID'), version = c(7.2)) {
+  org = match.arg(org)
+  id = match.arg(id)
+  version = match.arg(version)
+  
+  obj_name = paste0('msigdb.v', version, '.', org, '.', id)
+  gsc = do.call(obj_name, list())
+  return(gsc)
+}
+
 #' Subset collections and sub-collections of MSigDB
 #'
 #' The molecular signatures database (MSigDB) is composed of collections and
