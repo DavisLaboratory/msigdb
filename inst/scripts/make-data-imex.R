d86827d3a6f45c8df97cad29cf531e7eb9907637
bhuva.d <bhuva.d@wehi.edu.au>
2021-10-11 18:53:33 +1100

    Update PPI code and metadata

---
+++ inst/scripts/make-data-imex.R
@@ -0,0 +1,149 @@
+library(plyr)
+library(stringr)
+library(org.Hs.eg.db)
+library(org.Mm.eg.db)
+library(tidyverse)
+library(BiocFileCache)
+
+imex_date = '2021-07-06'
+bfc = BiocFileCache()
+imex_path = bfcrpath(bfc, sprintf('ftp://ftp.ebi.ac.uk/pub/databases/intact/%s/psimitab/intact-micluster.txt', imex_date))
+
+#----download PPI----
+#process human and mouse PPI
+ppi = read.csv(imex_path, sep = '\t', header = TRUE)
+ppi = ppi |> 
+  #find interactions determined within the same species
+  filter(Taxid.interactor.A == Taxid.interactor.B) |> 
+  #extract values
+  mutate(
+    Taxid = str_extract(Taxid.interactor.A, '[0-9]+')
+  ) |> 
+  #choose human and mouse only
+  filter(Taxid %in% c('9606', '10090')) |> 
+  mutate(
+    InteractionType = gsub('psi-mi:MI:[0-9]+\\(|\\)', '', Interaction.type.s.),
+    DetectionMethod = gsub('psi-mi:MI:[0-9]+\\(|\\)', '', Interaction.detection.method.s.)
+  ) |>
+  #extract values
+  mutate(
+    Confidence = as.numeric(str_remove(Confidence.value.s., '^.*:')),
+    InteractorA = str_remove(Alt..ID.s..interactor.A, '^.*:'),
+    InteractorB = str_remove(Alt..ID.s..interactor.B, '^.*:')
+  ) |> 
+  #map uniprot to entrez
+  mutate(
+    EntrezA = case_when(
+      Taxid == '9606' ~ mapIds(org.Hs.eg.db, InteractorA, 'ENTREZID', 'UNIPROT'),
+      Taxid == '10090' ~ mapIds(org.Mm.eg.db, InteractorA, 'ENTREZID', 'UNIPROT'),
+    ),
+    EntrezB = case_when(
+      Taxid == '9606' ~ mapIds(org.Hs.eg.db, InteractorB, 'ENTREZID', 'UNIPROT'),
+      Taxid == '10090' ~ mapIds(org.Mm.eg.db, InteractorB, 'ENTREZID', 'UNIPROT'),
+    )
+  ) |> 
+  filter(!is.na(EntrezA) & !is.na(EntrezB)) |> 
+  #map uniprot to symbol
+  mutate(
+    SymbolA = case_when(
+      Taxid == '9606' ~ mapIds(org.Hs.eg.db, EntrezA, 'SYMBOL', 'ENTREZID'),
+      Taxid == '10090' ~ mapIds(org.Mm.eg.db, EntrezA, 'SYMBOL', 'ENTREZID'),
+    ),
+    SymbolB = case_when(
+      Taxid == '9606' ~ mapIds(org.Hs.eg.db, EntrezB, 'SYMBOL', 'ENTREZID'),
+      Taxid == '10090' ~ mapIds(org.Mm.eg.db, EntrezB, 'SYMBOL', 'ENTREZID'),
+    )
+  ) |> 
+  distinct() |> 
+  #summarise information for each pair
+  select(
+    InteractorA,
+    InteractorB,
+    EntrezA,
+    EntrezB,
+    SymbolA,
+    SymbolB,
+    Taxid,
+    InteractionType,
+    DetectionMethod,
+    Confidence
+  ) |> 
+  mutate(Inferred = FALSE)
+
+#split human and mouse
+ppi_hs = ppi[ppi$Taxid %in% '9606', ]
+ppi_mm = ppi[ppi$Taxid %in% '10090', ]
+
+#----translate using homologs----
+ppi_hs_hcop = ppi_mm |>
+  left_join(msigdb:::hcop, by = c('EntrezA' = 'mouse_entrez_gene')) |> 
+  left_join(msigdb:::hcop, by = c('EntrezB' = 'mouse_entrez_gene')) |> 
+  ungroup() |>
+  select(!(EntrezA:EntrezB)) |>
+  rename(EntrezA = human_entrez_gene.x, EntrezB = human_entrez_gene.y) |>
+  mutate(
+    SymbolA = mapIds(org.Hs.eg.db, EntrezA, 'SYMBOL', 'ENTREZID'),
+    SymbolB = mapIds(org.Hs.eg.db, EntrezB, 'SYMBOL', 'ENTREZID'),
+    Taxid = '9606',
+    Inferred = TRUE
+  ) |>
+  distinct() |> 
+  as.data.frame()
+ppi_hs = rbind(ppi_hs, ppi_hs_hcop)
+
+ppi_mm_hcop = ppi_hs |>
+  left_join(msigdb:::hcop, by = c('EntrezA' = 'human_entrez_gene')) |> 
+  left_join(msigdb:::hcop, by = c('EntrezB' = 'human_entrez_gene')) |> 
+  ungroup() |>
+  select(!(EntrezA:EntrezB)) |>
+  rename(EntrezA = mouse_entrez_gene.x, EntrezB = mouse_entrez_gene.y) |>
+  mutate(
+    SymbolA = mapIds(org.Mm.eg.db, EntrezA, 'SYMBOL', 'ENTREZID'),
+    SymbolB = mapIds(org.Mm.eg.db, EntrezB, 'SYMBOL', 'ENTREZID'),
+    Taxid = '10090',
+    Inferred = TRUE
+  ) |>
+  distinct() |> 
+  as.data.frame()
+ppi_mm = rbind(ppi_mm, ppi_mm_hcop)
+rm(ppi_mm_hcop, ppi_hs_hcop)
+
+#----remove redundancry----
+ppi = rbind(ppi_hs, ppi_mm) |> distinct()
+
+ppi = ppi |> 
+  mutate(switch = as.numeric(EntrezA) > as.numeric(EntrezB)) |> 
+  mutate(
+    tmpB = EntrezB,
+    EntrezB = if_else(switch, EntrezA, EntrezB),
+    EntrezA = if_else(switch, tmpB, EntrezA),
+    tmpB = SymbolB,
+    SymbolB = if_else(switch, SymbolA, SymbolB),
+    SymbolA = if_else(switch, tmpB, SymbolA),
+    tmpB = InteractorB,
+    InteractorB = if_else(switch, InteractorA, InteractorB),
+    InteractorA = if_else(switch, tmpB, InteractorA)
+  ) |> 
+  distinct() |> 
+  group_by(EntrezA, EntrezB, Taxid) |> 
+  summarise(
+    InteractorA = paste(unique(InteractorA), collapse = '|'),
+    InteractorB = paste(unique(InteractorB), collapse = '|'),
+    SymbolA = paste(unique(SymbolA), collapse = '|'),
+    SymbolB = paste(unique(SymbolB), collapse = '|'),
+    InteractionType = paste(InteractionType, collapse = '|'),
+    DetectionMethod = paste(DetectionMethod, collapse = '|'),
+    Confidence = max(Confidence),
+    Inferred = all(Inferred)
+  ) |> 
+  ungroup()
+
+#remove duplicate evidence
+ppi$InteractionType = sapply(str_split(ppi$InteractionType, '\\|'), function(x) {
+  paste(unique(x), collapse = '|')
+})
+ppi$DetectionMethod = sapply(str_split(ppi$DetectionMethod, '\\|'), function(x) {
+  paste(unique(x), collapse = '|')
+})
+
+saveRDS(ppi, file = sprintf('imex_hsmm_%s.rds', format(as.Date(imex_date), '%m%y')))
--- inst/scripts/make-metadata.R
+++ inst/scripts/make-metadata.R
@@ -1,3 +1,12 @@
+#retrieve file list that need to be updated
+allfiles = list.files(pattern = '.rds$', recursive = TRUE)
+allfiles = setdiff(allfiles, basename(old_meta$RDataPath))
+old_meta = read.csv('inst/extdata/metadata.csv')
+msigdb_files = basename(grep('msigdb', allfiles, value = TRUE))
+imex_files = basename(grep('imex', allfiles, value = TRUE))
+meta = c()
+
+#----msigdb metadata----
 #params
 org_name = c('hs' = 'Homo sapiens', 'mm' = 'Mus musculus')
 org_taxid = c('hs' = 9606, 'mm' = 10090)
@@ -13,13 +22,8 @@ buildDescription <- function(ver, org, type) {
   }
 }
 
-#retrieve file list that need to be updated
-allfiles = list.files(pattern = '.rds$')
-old_meta = read.csv('inst/extdata/metadata.csv')
-allfiles = setdiff(allfiles, basename(old_meta$RDataPath))
-
-if (length(allfiles) > 0) {
-  meta = plyr::ldply(allfiles, function(x) {
+if (length(msigdb_files) > 0) {
+  msigdb_meta = plyr::ldply(msigdb_files, function(x) {
     #create regex
     fregex = 'msigdb.v([0-9]\\.[0-9])\\.([a-z]+)\\.(.*).rds$'
     
@@ -44,13 +48,34 @@ if (length(allfiles) > 0) {
       Maintainer = 'Dharmesh D. Bhuva <bhuva.d@wehi.edu.au>',
       RDataClass = 'GSEABase::GeneSetCollection',
       DispatchClass = 'Rds',
-      RDataPath = file.path('msigdb', x)
+      RDataPath = file.path('msigdb', paste0('msigdb.v', ver), x)
     )
   })
   
-  meta = rbind(old_meta, meta)
-  write.csv(meta, file = 'inst/extdata/metadata.csv', row.names = FALSE)
+  meta = rbind(meta, msigdb_meta)
 }
 
-#test metadata file
+#----IMEX metadata----
+##build record
+meta = rbind(meta, c(
+  Title = 'imex_hsmm_0721',
+  Description = 'Protein-protein interaction (PPI) network for human and mouse obtained from the international molecular exchange (IMEX) in July 2021.',
+  BiocVersion = as.numeric(as.character(BiocManager::version())) + 0.01,
+  Genome = NA,
+  SourceType = 'TXT',
+  SourceUrl = 'ftp://ftp.ebi.ac.uk/pub/databases/intact/2021-07-06/psimitab/intact-micluster.txt',
+  SourceVersion = '2021-07-06',
+  Species = 'Homo sapiens',
+  TaxonomyId = '9606',
+  Coordinate_1_based = TRUE,
+  DataProvider = 'EBI',
+  Maintainer = 'Dharmesh D. Bhuva <bhuva.d@wehi.edu.au>',
+  RDataClass = 'data.frame',
+  DispatchClass = 'Rds',
+  RDataPath = file.path('msigdb', 'IMEx', 'imex_hsmm_0721.rds')
+))
+
+#write and test metadata file
+meta = rbind(old_meta, meta)
+write.csv(meta, file = 'inst/extdata/metadata.csv', row.names = FALSE)
 ExperimentHubData::makeExperimentHubMetadata('../msigdb')
